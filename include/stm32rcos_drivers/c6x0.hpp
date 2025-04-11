29eef35941a6b6cbaec9b13f53cc2e25c870ec03
Rin Iwai <rin@eyrin.jp>
2025-04-12 01:36:48 +0900

    fix c6x0

--- include/stm32rcos_drivers/c6x0.hpp
+++ include/stm32rcos_drivers/c6x0.hpp
@@ -27,11 +27,9 @@ enum class C6x0ID {
   ID_8,
 };
 
-template <class CAN_> class C6x0;
-
-template <class CAN_> class C6x0Manager {
+class C6x0Manager {
 public:
-  C6x0Manager(CAN_ &can) : can_{can} {
+  C6x0Manager(stm32rcos::peripheral::CANBase &can) : can_{can} {
     can_.attach_rx_queue({0x200, 0x7F0, false}, rx_queue_);
   }
 
@@ -41,26 +39,23 @@ public:
     stm32rcos::peripheral::CANMessage msg;
     while (rx_queue_.pop(msg, 0)) {
       if (0x201 <= msg.id && msg.id < 0x201 + 8) {
-        C6x0<CAN_> *motor = motors_[msg.id - 0x201];
-        if (motor) {
-          int16_t position =
-              static_cast<int16_t>(msg.data[0] << 8 | msg.data[1]);
-          if (motor->prev_position_) {
-            int16_t delta = position - *motor->prev_position_;
-            if (delta > 4096) {
-              delta -= 8192;
-            } else if (delta < -4096) {
-              delta += 8192;
-            }
-            motor->position_ += delta;
-          } else {
-            motor->position_ = position;
+        Params &motor = params_[msg.id - 0x201];
+        int16_t position = static_cast<int16_t>(msg.data[0] << 8 | msg.data[1]);
+        if (motor.prev_position_) {
+          int16_t delta = position - *motor.prev_position_;
+          if (delta > 4096) {
+            delta -= 8192;
+          } else if (delta < -4096) {
+            delta += 8192;
           }
-          motor->prev_position_ = position;
-          motor->rpm_ = static_cast<int16_t>(msg.data[2] << 8 | msg.data[3]);
-          motor->current_ =
-              static_cast<int16_t>(msg.data[4] << 8 | msg.data[5]);
+          motor.position_ += delta;
+        } else {
+          motor.position_ = position;
         }
+        motor.prev_position_ = position;
+        motor.rpm_ = static_cast<int16_t>(msg.data[2] << 8 | msg.data[3]);
+        motor.current_raw_ =
+            static_cast<int16_t>(msg.data[4] << 8 | msg.data[5]);
       }
     }
   }
@@ -68,10 +63,8 @@ public:
   bool transmit() {
     stm32rcos::peripheral::CANMessage msg{0x200, false, 8};
     for (size_t i = 0; i < 4; ++i) {
-      if (motors_[i]) {
-        msg.data[i * 2] = motors_[i]->current_ref_ >> 8;
-        msg.data[i * 2 + 1] = motors_[i]->current_ref_;
-      }
+      msg.data[i * 2] = params_[i].current_ref_raw_ >> 8;
+      msg.data[i * 2 + 1] = params_[i].current_ref_raw_;
     }
     if (!can_.transmit(msg, 5)) {
       return false;
@@ -79,68 +72,79 @@ public:
     msg.data.fill(0);
     msg.id = 0x1FF;
     for (size_t i = 0; i < 4; ++i) {
-      if (motors_[i + 4]) {
-        msg.data[i * 2] = motors_[i + 4]->current_ref_ >> 8;
-        msg.data[i * 2 + 1] = motors_[i + 4]->current_ref_;
-      }
+      msg.data[i * 2] = params_[i + 4].current_ref_raw_ >> 8;
+      msg.data[i * 2 + 1] = params_[i + 4].current_ref_raw_;
     }
     return can_.transmit(msg, 5);
   }
 
-private:
-  CAN_ &can_;
-  stm32rcos::core::Queue<stm32rcos::peripheral::CANMessage> rx_queue_{64};
-  std::array<C6x0<CAN_> *, 8> motors_{};
-
-  friend C6x0<CAN_>;
-};
-
-template <class CAN_> class C6x0 {
-public:
-  C6x0(C6x0Manager<CAN_> &manager, C6x0Type type, C6x0ID id)
-      : manager_{manager}, type_{type}, id_{id} {
-    manager_.motors_[stm32rcos::core::to_underlying(id_)] = this;
+  int64_t get_position(C6x0ID id) {
+    return params_[stm32rcos::core::to_underlying(id)].position_;
   }
 
-  ~C6x0() { manager_.motors_[stm32rcos::core::to_underlying(id_)] = nullptr; }
+  int16_t get_rpm(C6x0ID id) {
+    return params_[stm32rcos::core::to_underlying(id)].rpm_;
+  }
 
-  int64_t get_position() { return position_; }
+  int16_t get_current_raw(C6x0ID id) {
+    return params_[stm32rcos::core::to_underlying(id)].current_raw_;
+  }
 
-  float get_rpm() { return rpm_; }
+  void set_current_ref_raw(C6x0ID id, int16_t current) {
+    params_[stm32rcos::core::to_underlying(id)].current_ref_raw_ = current;
+  }
+
+private:
+  struct Params {
+    int64_t position_ = 0;
+    std::optional<int16_t> prev_position_;
+    int16_t rpm_ = 0;
+    int16_t current_raw_ = 0;
+    int16_t current_ref_raw_ = 0;
+  };
+
+  stm32rcos::peripheral::CANBase &can_;
+  stm32rcos::core::Queue<stm32rcos::peripheral::CANMessage> rx_queue_{64};
+  std::array<Params, 8> params_{};
+};
+
+class C6x0 {
+public:
+  C6x0(C6x0Manager &manager, C6x0Type type, C6x0ID id)
+      : manager_{manager}, type_{type}, id_{id} {}
+
+  ~C6x0() { set_current_ref(0); }
+
+  int64_t get_position() { return manager_.get_position(id_); }
+
+  int16_t get_rpm() { return manager_.get_rpm(id_); }
 
   float get_current() {
     switch (type_) {
     case C6x0Type::C610:
-      return current_;
+      return manager_.get_current_raw(id_);
     case C6x0Type::C620:
-      return current_ / 16384.0f * 20000.0f;
+      return manager_.get_current_raw(id_) / 16384.0f * 20000.0f;
     }
   }
 
-  void set_current(float current) {
+  void set_current_ref(float current) {
     switch (type_) {
     case C6x0Type::C610:
-      current_ref_ = std::clamp(current, -10000.0f, 10000.0f);
+      manager_.set_current_ref_raw(id_,
+                                   std::clamp(current, -10000.0f, 10000.0f));
       break;
     case C6x0Type::C620:
-      current_ref_ =
-          std::clamp(current, -20000.0f, 20000.0f) / 20000.0f * 16384.0f;
+      manager_.set_current_ref_raw(
+          id_, std::clamp(current, -20000.0f, 20000.0f) / 20000.0f * 16384.0f);
       break;
     }
   }
 
 private:
-  C6x0Manager<CAN_> &manager_;
+  C6x0Manager &manager_;
   C6x0Type type_;
   C6x0ID id_;
-
-  int64_t position_ = 0;
-  std::optional<int16_t> prev_position_;
-  int16_t rpm_ = 0;
-  int16_t current_ = 0;
-  int16_t current_ref_ = 0;
-
-  friend C6x0Manager<CAN_>;
 };
 
 } // namespace stm32rcos_drivers
